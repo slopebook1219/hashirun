a372c210cdfeee2b6920eded5f51776849ba3fc8
坂本大幹 <saka.ttt.1219@gmail.com>
2026-04-25 10:27:27 +0900

    init spring project

---
+++ src/main/java/com/example/hashirun/HashirunApplication.java
@@ -0,0 +1,13 @@
+package com.example.hashirun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashirunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashirunApplication.class, args);
+	}
+
+}
